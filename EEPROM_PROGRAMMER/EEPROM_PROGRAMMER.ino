59fc0bad967a6f2e9fc7a15d0d4e888f042590a9
UnPolacoLoco <31392246+UnPolacoLoco@users.noreply.github.com>
2017-11-01 19:13:12 +0100

    changes to support 4 digit decimal displays

--- EEPROM_PROGRAMMER/EEPROM_PROGRAMMER.ino
+++ EEPROM_PROGRAMMER/EEPROM_PROGRAMMER.ino
@@ -31,21 +31,60 @@ void setup() {
   pinMode(SR_CLK, OUTPUT);
 
   // 4-bit hex decoder for common cathode 7-segment display
-  byte data[] = { 0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b, 0x77, 0x1f, 0x4e, 0x3d, 0x4f, 0x47 };
+  byte digits[] = { 0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b}; //0x77, 0x1f, 0x4e, 0x3d, 0x4f, 0x47 }
 
   // 4-bit hex decoder for common anode 7-segment display
   // byte data[] = { 0x81, 0xcf, 0x92, 0x86, 0xcc, 0xa4, 0xa0, 0x8f, 0x80, 0x84, 0x88, 0xe0, 0xb1, 0xc2, 0xb0, 0xb8 };
 
 
-  dataLenght = ArrayLenght(data);
-
-  for (int i = 0; i < dataLenght; i++)
-  {
-    writeEEPROM(i, data[i]);
-  }
 
   Serial.begin(9600);
+
+  Serial.println("Starting EEPROM writing routines, this might take a while, please wait...");
+
+  for (int value = 0; value < 256; value++)
+  {
+    writeEEPROM(value, digits[value % 10]);
+  }
+  Serial.print(".");
+  for (int value = 0; value < 256; value++)
+  {
+    writeEEPROM(value + 256, digits[(value / 10) % 10]);
+  }
+  Serial.print(".");
+  for (int value = 0; value < 256; value++)
+  {
+    writeEEPROM(value + 512, digits[(value / 100)]);
+  }
+  Serial.print(".");
+  for (int value = 0; value < 256; value++)
+  {
+    writeEEPROM(value + 768, 0);
+  }
+  Serial.println(".");
+
+  Serial.println("Operation successful!");
+
+  //  for (int i = 0; i < dataLenght; i++)
+  //  {
+  //    writeEEPROM(i, data[i]);
+  //  }
+
+  Serial.println("Testing...");
+
+  for (int address = 0; address < ArrayLenght(digits); address++)
+  {
+    Serial.print("0x0");
+    Serial.print(address, HEX);
+    if (readEEPROM(address) == digits[address])
+      Serial.println(" success!");
+
+    else
+      Serial.print(" error!");
+  }
   showEEPROM();
+
+
 
 }
 
@@ -69,26 +108,12 @@ void setAddress(int address, bool outputEnable)
 
 }
 
-void enableOutput() /* ensures write enable is disabled and output is enabled */
-{
-  digitalWrite(WRITE_EN, HIGH);
-  int outputIO = 0;
 
-  shiftOut(DATA_PIN, SR_CLK, LSBFIRST, outputIO);
-  shiftOut(DATA_PIN, SR_CLK, LSBFIRST, (outputIO >> 8));
-
-  digitalWrite(LATCH, 0);
-  digitalWrite(LATCH, 1);
-  digitalWrite(LATCH, 0);
-
-  delay(10);
-
-}
 
 
 void showEEPROM()
 {
-  for (int address = 0; address <= dataLenght; address++)
+  for (int address = 0; address < 1024; address++)
   {
     Serial.print("Address: 0x");
     Serial.print(address, HEX);
@@ -104,9 +129,13 @@ void writeEEPROM(int address, byte data)
 {
   setAddress(address, false);
 
+
+
+
   for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin++)
   {
     pinMode(pin, OUTPUT);
+
   }
 
   for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin++)
